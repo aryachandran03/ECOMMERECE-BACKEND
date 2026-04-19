9a1ec86b6ebcdbc1955ad491053e6840b57986ee
aryachandran03 <aryachandran03@gmail.com>
2026-04-20 01:12:18 +0400

    Implemented authentication with bcrypt and JWT

---
+++ controllers/authentication.js
@@ -0,0 +1,29 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  const hashed = await bcrypt.hash(req.body.password, 10);
+
+  const user = await User.create({
+    ...req.body,
+    password: hashed
+  });
+
+  res.json(user);
+};
+
+exports.login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  const match = await bcrypt.compare(req.body.password, user.password);
+
+  if (!match) return res.json({ message: "Invalid credentials" });
+
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET
+  );
+
+  res.json({ token });
+};
